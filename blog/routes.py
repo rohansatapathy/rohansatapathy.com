cca7bbb7559e8f673540fa8222829e0f490fc4f6
Rohan Satapathy <satapathyrohan13@gmail.com>
2021-07-26 11:50:30 -0500

    Add navbar

--- blog/routes.py
+++ blog/routes.py
@@ -1,8 +1,24 @@
 from flask import render_template, redirect, url_for
 
-from blog import app
+from blog import app, pages
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/<path:path>/')
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+
+@app.route('/about_me/')
+def about_me():
+    return "About Me"
+
+
+@app.route('/posts/')
+def posts():
+    return "Posts"
